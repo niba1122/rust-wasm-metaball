60e9b58347f8b6b9b73bd068c7c6a956625245ee
Nobuhito Ibaraki <nobuhito.ibaraki@zozo.com>
2019-12-06 21:53:30 -0800

    https://wgld.org/d/glsl/g009.html

--- src/lib.rs
+++ src/lib.rs
@@ -26,14 +26,45 @@ extern "C" {
 static FRAGMENT_SHADER: &'static str = r#"
 precision mediump float;
 uniform float time;
-uniform vec2 mouse;
-uniform vec2 resolution;
+uniform vec2  mouse;
+uniform vec2  resolution;
+
+const float sphereSize = 1.0; // 球の半径
+
+float distanceFunc(vec3 p){
+    return length(p) - sphereSize;
+}
 
 void main(void){
-    vec2 m = vec2(mouse.x * 2.0 - 1.0, -mouse.y * 2.0 + 1.0);
+    // fragment position
     vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / min(resolution.x, resolution.y);
-    float t = sin(length(m - p) * 30.0 + time * 5.0);
-    gl_FragColor = vec4(vec3(t), 1.0);
+    
+    // camera
+    vec3 cPos = vec3(0.0,  0.0,  2.0);
+    vec3 cDir = vec3(0.0,  0.0, -1.0);
+    vec3 cUp  = vec3(0.0,  1.0,  0.0);
+    vec3 cSide = cross(cDir, cUp);
+    float targetDepth = 1.0;
+    
+    // ray
+    vec3 ray = normalize(cSide * p.x + cUp * p.y + cDir * targetDepth);
+    
+    // marching loop
+    float distance = 0.0; // レイとオブジェクト間の最短距離
+    float rLen = 0.0;     // レイに継ぎ足す長さ
+    vec3  rPos = cPos;    // レイの先端位置
+    for(int i = 0; i < 16; i++){
+        distance = distanceFunc(rPos);
+        rLen += distance;
+        rPos = cPos + ray * rLen;
+    }
+    
+    // hit check
+    if(abs(distance) < 0.001){
+        gl_FragColor = vec4(vec3(1.0), 1.0);
+    }else{
+        gl_FragColor = vec4(vec3(0.0), 1.0);
+    }
 }
 "#;
 
